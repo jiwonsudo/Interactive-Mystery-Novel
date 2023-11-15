9b3272887fa1f7953f2b2f677061bcd5836efd2a
jiwonsudo <wondev112@gmail.com>
2023-11-16 02:51:02 +0900

    Feat: Complete Showing storyData feature

--- src/Components/Contents/index.jsx
+++ src/Components/Contents/index.jsx
@@ -1,40 +1,90 @@
 import styled from 'styled-components';
-import stories from '../../data/stories.json';
+import { useEffect, useState } from 'react';
+import storiesData from '../../data/stories.json';
+
+const MainText = styled.div`
+  margin: 30px 15px 0;
+  color: #000000;
+  font-family: 'NanumMyeongjo', serif;
+  font-size: 1em;
+  line-height: 1.5em;
+`;
+
+const Button = styled.button`
+  width: 40vw;
+  max-width: 300px;
+  padding: 14px 25px;
+  border-radius: 15px;
+  border: 1px solid #858585;
+  background: ${(props) => props.$bgColor ? props.$bgColor : '#FFFFFF'};
+  color: ${(props) => props.$color ? props.$color : '#FFFFFF'};
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  font-family: 'NanumSquareNeo';
+  font-size: 0.7em;
+  line-height: 0.8em;
+  display: ${(props) => props.$isHidden ? 'none' : 'block'};
+`;
+
+const ButtonContainer = styled.div`
+  margin: 30px 15px 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 function Contents() {
-  const btnBlueColor = '#0C3E9D';
-  const btnGrayColor = '#C9C9C9';
+  const btnBlueBgColor = '#0C3E9D';
+  const btnGrayBgColor = '#C9C9C9';
+  const btnBlueColor = '#FFFFFF';
+  const btnGrayColor = '#FFFFFF';
+  
+  const STORY_DATA_URI = '../../data/stories.json';
 
-  let btnBlueContent = 'testBlueText';
-  let btnGrayContent = 'testGrayText';
+  const [mainContent, setMainContent] = useState('눈에 갇힌 외딴 산장에서 - 히가시노 게이고');
+  const [userChoices, setUserChoices] = useState([]);  // use 'A' for choosing gray, use 'B' for choosing blue.
+  const [btnBlueContent, setBtnBlueContent] = useState('파란 버튼 테스트 글');
+  const [btnGrayContent, setBtnGrayContent] = useState('회색 버튼 테스트 글');
 
-  const MainText = styled.div`
-    margin: 30px 15px 0;
-    color: #000000;
-    font-family: 'NanumMyeongjo', serif;
-  `;
+  const [hideButtons, setHideButtons] = useState(false);
 
-  const Button = styled.button`
-    padding: 14px 25px;
-    border-radius: 15px;
-    border: 1px solid #858585;
-    background: ${(props) => props.color ? props.color : '#FFFFFF'};
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  `;
+  function getDataByPartialKey(storiesData, partialKey) {
+    const matchingKey = Object.keys(storiesData).find(key => key.includes(partialKey));
 
-  const ButtonContainer = styled.div`
-    margin: 30px 15px 0;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-  `;
+    if (matchingKey) {
+      return storiesData[matchingKey];
+    } else {
+      return null;
+    }
+  }
+
+  function updateContents(props) {
+    if (props.target.id === 'btnGray') {
+      setUserChoices([...userChoices, 'A']);
+    } else if (props.target.id === 'btnBlue') {
+      setUserChoices([...userChoices, 'B']);
+    }
+  }
+
+  useEffect(() => {
+    console.log(userChoices);
+    const storyKey = `story${userChoices.join('')}`;
+    const storyData = getDataByPartialKey(storiesData, storyKey);
+    setMainContent(storyData.main);
+
+    if (storyData.end === true) {
+      setHideButtons(true);
+    } else {
+      setBtnGrayContent(storyData.choiceA);
+      setBtnBlueContent(storyData.choiceB);
+    }
+  }, [userChoices]);
 
   return (
     <>
-      <MainText>컨텐츠들어가는곳. 여기에. 컨텐츠. 들어감. 이렇게. 글 여러번 써도.</MainText>
+      <MainText>{mainContent}</MainText>
       <ButtonContainer>
-        <Button color={btnGrayColor}>{btnBlueContent}</Button>
-        <Button color={btnBlueColor}>{btnGrayContent}</Button>
+        <Button id='btnGray' $bgColor={btnGrayBgColor} $color={btnGrayColor} $isHidden={hideButtons} onClick={props => updateContents(props)}>{btnGrayContent}</Button>
+        <Button id='btnBlue' $bgColor={btnBlueBgColor} $color={btnBlueColor} $isHidden={hideButtons} onClick={props => updateContents(props)}>{btnBlueContent}</Button>
       </ButtonContainer>
     </>
   );
